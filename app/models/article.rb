13935dcdb0ec4b17a70ca60abfae6086cbdddc5f
Mike <mikemakol45@gmail.com>
2023-07-09 19:53:12 +0300

    update file

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
+require_relative './author.rb'
+require_relative './magazine.rb'
 class Article
+    attr_reader :title, :author, :magazine
+    @@all = []
 
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
